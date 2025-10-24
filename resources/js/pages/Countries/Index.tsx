ff9dbaa641fda746cc8263c8b9665e55d59ce08c
Rodrigo Alvarez <r.alvarezcobarruvias@gmail.com>
2025-10-24 16:38:55 -0700

    Search functionality works

--- resources/js/pages/Countries/Index.tsx
+++ resources/js/pages/Countries/Index.tsx
@@ -1,7 +1,8 @@
-import { Head } from '@inertiajs/react';
+import { Head, router } from '@inertiajs/react';
 import { Button } from 'primereact/button';
 import { Card } from 'primereact/card';
 import { InputText } from 'primereact/inputtext';
+import { ChangeEvent, useState } from 'react';
 import CardHeader from './Components/CardHeader';
 
 interface Props {
@@ -9,21 +10,38 @@ interface Props {
 }
 
 const Index = ({ countries }: Props) => {
+    const [searchTerm, setSearchTerm] = useState<string | null>(null);
+
+    const handleSearch = () => {
+        router.get('/', { search: searchTerm }, { preserveState: true });
+    };
+
     return (
         <>
             <Head title="Countries" />
             <div className="mx-auto max-w-[1140px]">
                 <div className="mx-4 my-4">
                     <div className="p-inputgroup">
-                        <InputText placeholder="Keyword" />
-                        <Button icon="pi pi-search" />
+                        <InputText
+                            placeholder="Keyword"
+                            onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+                                if (e.key === 'Enter') {
+                                    handleSearch();
+                                }
+                            }}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)}
+                        />
+                        <Button icon="pi pi-search" onClick={handleSearch} />
                     </div>
                 </div>
                 <div className="mx-4 mt-12 grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
                     {countries.map((country) => (
-                        <div className="cursor-pointer hover:brightness-90">
-                            <Card key={country.cca3} title={country.name.common} header={<CardHeader country={country} />} />
-                        </div>
+                        <Card
+                            className="cursor-pointer hover:brightness-90"
+                            key={country.cca3}
+                            title={country.name.common}
+                            header={<CardHeader country={country} />}
+                        />
                     ))}
                 </div>
             </div>
