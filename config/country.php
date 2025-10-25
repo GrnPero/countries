32e9bffcdfb5eaf92ef2597896ab43e0e37cf9a4
Rodrigo Alvarez <r.alvarezcobarruvias@gmail.com>
2025-10-24 20:47:21 -0700

    Fixed the default URL

--- config/country.php
+++ config/country.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'base_url' => env('REST_COUNTRIES_API_URL', 'https://restcountries.com/v3.1/all'),
+    'base_url' => env('REST_COUNTRIES_API_URL', 'https://restcountries.com/v3.1/'),
 ];
